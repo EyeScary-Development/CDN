74a345a98d7b388e4b4249036395908f2a6bc871
Peter Rimmer <120305267+Pittab@users.noreply.github.com>
2025-01-16 11:25:19 +0000

    Update install.sh

--- agpm_packages/stronge/protocols/install.sh
+++ agpm_packages/stronge/protocols/install.sh
@@ -6,12 +6,22 @@ else
 fi
 cd ~/.agpm
 if [[ -d "$(eval echo "~/.agpm/stronge")" ]]; then
-    echo "Stronge directory exists already, try update instead"
+    echo "Package directory exists already, try update instead"
     exit
 else
-    echo "Stronge not installed, installing..."
+    echo "Package not installed, installing..."
     mkdir ~/.agpm/stronge && cd ~/.agpm/stronge
     curl -O https://eyescary-development.github.io/CDN/agpm_packages/stronge/package.zip
     unzip package.zip
     rm package.zip
+    if [ "$SHELL" == "/bin/zsh" ]; then
+      export file="$HOME/.zshrc"
+    elif [ "$SHELL" == "/bin/bash" ]; then
+      export file="$HOME/.bashrc"
+    elif ["$SHELL" == "/bin/fish"]; then
+      export file="$HOME/.config/fish/config.fish"
+    else
+      echo "Your shell isn't supported by the autoinstaller, create any aliases manually"
+    fi
+    echo "Package installed successfully!"
 fi
