e68ac9be9db7b938b066e42e4d39d00a7b46fd73
Pittab <120305267+Pittab@users.noreply.github.com>
2025-01-17 07:51:43 +0000

    added an package

---
+++ agpm_packages/estodo/protocols/update.sh
@@ -0,0 +1,17 @@
+if [[ -d "$(eval echo "~/.agpm")" ]]; then
+    echo "Agpm directory exists"
+else
+    echo "Agpm directory doesn't exist, package therefore cannot be installed. quitting..."
+    exit
+fi
+cd ~/.agpm
+if [[ -d "$(eval echo "~/.agpm/estodo")" ]]; then
+    echo "Package directory exists. Proceeding..."
+    cd ~/.agpm/harmony/ 
+    rm *
+    curl -O https://eyescary-development.github.io/CDN/agpm_packages/estodo/package.zip
+    unzip package.zip
+    rm package.zip
+else
+    echo "Package not installed. Task failed successfully. Quitting..."
+fi
